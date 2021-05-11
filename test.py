d8a521b9901a01aef4177a81f6ff0bedfc8d0a9a
Niklas <niklas.munkes@t-online.de>
2021-05-11 17:23:42 +0200

    cleanup of existing code and added fact(n)

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 def sqr(n=int):
     acc = 0
-    while(True):
-        if (acc == n**2):
+    while True:
+        if acc == n ** 2:
             return acc
         acc += 1
+
+
+def fact(n):
+    if n < 0:
+        Exception("Invalid input")
+    elif n > 1:
+        return n * fact(n - 1)
+    else:
+        return 1
+
+
+print("Fact(6) = " + str(fact(6)))
+print("Fact(0) = " + str(fact(0)))
+print("Fact(-6) = " + str(fact(-6)))
 
 print(sqr(5))
 
