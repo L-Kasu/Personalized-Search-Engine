b3690dea720e098ed40bcb943588da6287c09366
L-Kasu <lars@kasueschke.de>
2021-05-13 17:47:04 +0200

    Added tokenizer to pp_funtions_clean

--- pp_functions_clean.py
+++ pp_functions_clean.py
@@ -1,6 +1,8 @@
 # clean function container for preprocessing_clean.py
 # author: Niklas Munkes
+import string
 
+from nltk.tokenize import RegexpTokenizer
 dir_containers = "./PP_Containers/"
 dir_archive = "./CISI_archive/"
 
@@ -33,3 +35,28 @@ def createTAWContainer(filename):
                 taw_container.write("\n")
         container.close()
     taw_container.close()
+
+def tokenize(filename):
+    tokenized = []
+    with open(dir_containers + filename, 'r') as container:
+        for line in container.readlines():
+            if line.startswith(".T") \
+                or line.startswith(".A")\
+                or line.startswith(".W"):
+                line = line[3:]
+            line = line.strip()
+            toker = RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps=True)
+            tokenized_line = toker.tokenize(line)
+            while ' ' in tokenized_line:
+                tokenized_line.remove(' ')
+            tokenized += tokenized_line
+
+    return tokenized
+
+print(tokenize("pp_container_T-A-W_CISI.ALL.txt"))
+
+
+
+
+
+
