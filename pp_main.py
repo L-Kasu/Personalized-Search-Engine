9b1094041e27a1a6dbd790ca9de153eeae215065
Niklas <niklas.munkes@t-online.de>
2021-05-15 16:30:18 +0200

    a1.251 easter egg (Dark Souls 3 ref) has now expanded screen time

--- pp_main.py
+++ pp_main.py
@@ -1,5 +1,5 @@
 # preprocessing function caller
-# version: alpha1.25
+# version: alpha1.251
 # authors: Niklas Munkes, Lars Kasüschke
 
 import sys
@@ -57,6 +57,29 @@ elif i == "2":
     stemmer = "lancaster"
 elif i == "273": # base damage of the Pontiff Knight Curved Sword
     stemmer = "sulyvahn"
+    print("Thank you for choosing the Sulyvahn Stemmer. Please stand by while the Outrider Knights are dispatched.")
+    print("If you like the Sulyvahn Stemmer, please consider leaving a positive review.\nIf you write it now, I'll make sure it is featured in the next issue of the Irithyll Morning Post.\nDo you want to write a review now (y/n)?")
+    print('> ', end='')
+    j = input()
+    if j == "y":
+        print('Your review:\n> ', end='')
+        usr_review = input()
+        print("Thank you!\n")
+    elif j == "n":
+        print(":(\n")
+    elif j != "n":
+        tb = sys.exc_info()[2]
+        raise Exception("Stop wasting my time, you impudent fool! Type either 'y' or 'n'").with_traceback(tb)
+    print("Should you stumble upon any bugs, something is not working or you just want to say hi, hit me up at freshpontiffsulyvahn89[at]profaned-flame-online.gov")
+    print("Have a nice day :)")
+    print("Cheers,")
+    print("Pontiff Sulyvahn")
+    print("\nContinue with the preprocessing (y/n)?\n> ", end='')
+    k = input()
+    if k == "y":
+        pass
+    else:
+        exit()
 else:
     tb = sys.exc_info()[2]
     raise Exception("Invalid input. Type either '1' or '2'").with_traceback(tb)
