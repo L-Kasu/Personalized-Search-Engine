4ecacc3633089d33b1935068ef5dd50a9800cc7a
Hannah <hannah.kessel@protonmail.ch>
2021-05-16 12:02:42 +0200

    chaged to using the pp_file_reader.py

--- inverted_matrix.py
+++ inverted_matrix.py
@@ -1,3 +1,6 @@
+import pp_file_reader as pp
+
+
 # builds the matrix with stemming and stopword removal
 def build_matrix_with_stemming():
     matrix = inverted_matrix('PP_output/pp_output_tnwsl_CISI.ALL.txt')
@@ -22,41 +25,11 @@ def frequenzy_mapping(matrix):
 # matrix[i][0] is the word
 # matrix[i][1] is the list of documents containing the word
 def inverted_matrix(file):
-
-    collection = extract_words(file)
     matrix = []
-    for lists in collection:
-        add_to_matrix(lists[0], lists[1], matrix)
+    collection = pp.file_reader(file)
+    for x in collection:
+        add_to_matrix(x['I'], x['W'], matrix)
     return matrix
-
-
-# extracts the words of a preprocessed collection into a collection list, consisting of a list for every document
-# the list of a document consists of the doc_id and the list of words containing it
-# input file: the preprocessed collection as txt file
-def extract_words(file):
-    with open(file, "r") as tf:
-        lines = tf.read().split('\n')
-
-    collection = []
-    wordlist = []
-    document = []
-    for line in lines:
-        if line.startswith(".I"):
-            doc_id = line.split(" ")[1].strip()
-            document = [doc_id]
-        elif line.startswith(".X"):
-            document.append(wordlist)
-            collection.append(document)
-            wordlist = []
-        else:
-            text = line.strip()[3:] + " "   # The first 3 characters of a line can be ignored.
-            text = text.strip()[1:-1]   # {,} removed
-            words = text.split(",")
-            for word in words:
-                word = word.strip()[1:-1]
-                if word not in wordlist:
-                    wordlist.append(word)
-    return collection
 
 
 # gets an list of words and the doc_id of the document containing the words and a inverted Matrix
