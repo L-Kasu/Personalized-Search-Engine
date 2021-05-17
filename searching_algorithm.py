c41d7d134bfb4543d18bc30bb23ef520fea4c0ce
JuliBD <83228693+JuliBD@users.noreply.github.com>
2021-05-17 17:37:15 +0200

    Update searching_algorithm.py
    adjusted to the return of "inverted_matrix.build_matrix_with_stemming()" now being a dict

--- searching_algorithm.py
+++ searching_algorithm.py
@@ -43,15 +43,7 @@ assert intersect(p2,p3) == [(77)]
 # searches for all documents containing all words in the given list
 
 def and_search(words):
-    matrix = inverted_matrix.build_matrix_with_stemming()
-
-    # convert matrix to dictionary
-    dict = {}
-    for i in range(0, len(matrix)):
-        key = matrix[i][0]
-        value = matrix[i][1]
-        dict[key] = value
-
+    dict = inverted_matrix.build_matrix_with_stemming()
 
     # gather all postings
     postings = []
