ce71d258ab4b0833ad020f91ccefcc2231b7797a
JuliBD <juliandoehl@googlemail.com>
2021-05-17 16:43:43 +0200

    Removed redundant code.

--- searching_algorithm.py
+++ searching_algorithm.py
@@ -1,12 +1,11 @@
+import inverted_matrix
+import pp_file_reader as pp
 
 # gets two postings
 # returns a posting
 # intersects two postings (only returns numbers contained in both postings)
 
 def intersect(p1,p2):
-    if len(p1) == 0 or len(p2) == 0:
-        return []
-
     answer = []
 
     pos1, pos2 = 0, 0
@@ -24,10 +23,10 @@ def intersect(p1,p2):
     return answer
 
 
+# testing
 p1 = [1, 2, 4 ,7 ,9]
 p2 = [2, 7, 13, 77]
 p3 = [5, 24, 25, 35, 77, 99]
-
 
 assert intersect(p1,p1) == p1
 assert intersect(p3,p3) == p3
@@ -36,8 +35,8 @@ assert intersect(p1,p3) == []
 assert intersect(p2,p3) == [(77)]
 
 
-print("intersect(", p1, ",", p2, ") =",intersect(p1,p2))
-print("intersect(", p1, ",", p3, ") =",intersect(p1,p3))
+#print("intersect(", p1, ",", p2, ") =",intersect(p1,p2))
+#print("intersect(", p1, ",", p3, ") =",intersect(p1,p3))
 
 
 # gets list of words
@@ -45,7 +44,7 @@ print("intersect(", p1, ",", p3, ") =",intersect(p1,p3))
 # searches for all documents containing all words in the given list
 
 def and_search(words):
-    # todo
+    matrix = inverted_matrix.build_matrix_with_stemming()
 
     # convert matrix to dictionary
     dict = {}
@@ -66,7 +65,5 @@ def and_search(words):
 
     for i in range(1, len(postings)):
         documents = intersect(documents,postings[i])
-
-
 
     return documents
