323662ce497a48c51ed7e7ee6346c66e19952ffc
L-Kasu <lars@kasueschke.de>
2021-06-02 11:13:59 +0200

    database integration (pickle library)

---
+++ database.py
@@ -0,0 +1,20 @@
+import pickle
+
+matrix = object
+
+
+def save_object(obj, name):
+    try:
+        with open("./data/" + name + ".pickle", "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error during pickling object (Possibly unsupported):", ex)
+
+
+def load_object(filename="data"):
+    try:
+        with open("./data/" + filename + ".pickle", "rb") as f:
+            return pickle.load(f)
+    except Exception as ex:
+        print("Error during unpickling object (Possibly unsupported):", ex)
+
