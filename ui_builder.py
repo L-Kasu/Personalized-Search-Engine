d4060328afca81a3eb56d9ec0c9dcecdc0dcf4dd
Niklas <niklas.munkes@t-online.de>
2021-06-04 18:50:15 +0200

    some code for the gtk script

--- ui_builder.py
+++ ui_builder.py
@@ -2,3 +2,11 @@
 # version: alpha0.1
 # author: Niklas Munkes
 
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+window = Gtk.Window(title="Hello World")
+window.show()
+window.connect("destroy", Gtk.main_quit)
+Gtk.main()
