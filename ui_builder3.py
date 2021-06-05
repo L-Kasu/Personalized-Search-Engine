044c10af3397d7cbcb3e213d7a17a36f6b58b075
musicalwillow <83435286+musicalpillow@users.noreply.github.com>
2021-06-05 23:02:00 +0200

    Moving code to GUI branch

---
+++ ui_builder3.py
@@ -0,0 +1,109 @@
+# script for a simple ui
+# version: alpha0.1
+# author: Haitham Samaan
+
+from tkinter import *
+
+
+# Selects the directory the user wants to search in
+def select_dir():
+    pass
+
+
+# Returns the file size the user chose
+def get_file_size():
+    return file_size_scale.get()
+
+
+# Preprocesses based on the search settings
+def preprocess():
+    pass
+
+
+# Filters the search based on the user's choice of file types
+def file_type():
+    if pdf.get() == 1:
+        pass
+    elif txt.get() == 1:
+        pass
+    elif docx.get() == 1:
+        pass
+    pass
+
+
+# Gets user input
+def user_entry():
+    return search_entry.get()
+
+
+# instantiating a Window:
+window = Tk()
+window.geometry("800x400")
+window.title("Search Engine")
+
+# Allows the user to select the directory they want to search in
+select_directory = Button(window,
+                          text="Select Directory",
+                          command=select_dir)
+select_directory.pack(side=TOP, anchor=NW)
+
+# Used many frames here to organize the different widgets better
+# a settings frame including: options for selecting file size and type
+settings_frame = Frame(window)
+settings_frame.pack(side=TOP)
+
+settings_label = Label(settings_frame,
+                       text="Settings",
+                       font=("Arial", 20, "bold"))
+settings_label.pack()
+
+file_size_frame = Frame(settings_frame)
+file_size_frame.pack()
+
+file_size_label = Label(file_size_frame,
+                        text="Pick file size (in MB)",
+                        font=("Arial", 10, "bold"))
+file_size_label.pack()
+
+file_size_scale = Scale(settings_frame,
+                        from_=0,
+                        to=200,
+                        length=150,
+                        orient=HORIZONTAL)
+file_size_scale.pack()
+
+file_type_frame = Frame(settings_frame)
+file_type_frame.pack()
+
+file_type_label = Label(file_type_frame,
+                        text="Select file type:",
+                        font=("Arial", 10, "bold"))
+file_type_label.pack()
+
+pdf = IntVar()
+file_type_pdf = Checkbutton(file_type_frame, text="PDF", variable=pdf)
+file_type_pdf.pack(side=LEFT)
+
+txt = IntVar()
+file_type_txt = Checkbutton(file_type_frame, text="TXT", variable=txt)
+file_type_txt.pack(side=LEFT)
+
+docx = IntVar()
+file_type_docx = Checkbutton(file_type_frame, text="DOCX", variable=docx)
+file_type_docx.pack(side=LEFT)
+
+preprocess_button = Button(settings_frame, text="preprocess", command=preprocess)
+preprocess_button.pack(side=BOTTOM)
+
+# Entry frame
+entry_frame = Frame(window)
+entry_frame.pack()
+
+search_entry = Entry(entry_frame)
+search_entry.pack(side=TOP)
+
+search_button = Button(entry_frame, text="Search")
+search_button.pack(side=BOTTOM)
+
+
+window.mainloop()
