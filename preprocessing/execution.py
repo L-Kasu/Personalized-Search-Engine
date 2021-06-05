d2a4d4e754fadd89eea3368e9bc523f2c02fce91
L-Kasu <lars@kasueschke.de>
2021-06-05 04:32:24 +0200

    rework preprocessing.execution

--- preprocessing/execution.py
+++ preprocessing/execution.py
@@ -1,80 +1,20 @@
 # preprocessing function container
 # authors: Niklas Munkes, Lars Kasüschke
 
-import sys
+import re
 import time
 import nltk
 from preprocessing import text_processing
 from preprocessing import sulyvahn
 from preprocessing import pp_main as pp
 from data import database
-taskstring_dict = { "t" : text_processing.tokenize,
-                    "n" : text_processing.normalize,
-                    "w" : text_processing.remove_stop_words,
-                    "p" : lambda s: text_processing.stemming(s, "porter"),
-                    "l" : lambda s: text_processing.stemming(s, "lancaster"),
-                    "v" : lambda s: text_processing.stemming(s, "sulyvahn")}
 
-
-def is_taskstring_valid(taskstring) -> bool:
-    if taskstring == "":
-        return True
-    else:
-        return type(taskstring) == str\
-               and taskstring[0] in taskstring_dict\
-               and is_taskstring_valid(taskstring[1:])
-
-
-def inline(container):
-    result = []
-    for line in container:
-        if line.startswith("."):
-            result.append(line.replace("\n", " "))
-        else:
-            if result:
-                result[-1] += line.replace("\n", " ")
-    return result
-
-
-def inline_for_REL(container):
-    result = {}
-    for line in container:
-        split = line.split()
-        split_0 = int(split[0]) -1
-        split_1 = int(split[1]) -1
-        if split_0 in result:
-            result[split_0].append(split_1)
-        else:
-            result[split_0] = [split_1]
-    return result
-
-
-def pre_processor(taskstring: str, filename: str) -> None:
-    with open(pp.dir_archive + filename, 'r') as container:
-        preprocessed_items = []
-        if(filename.endswith(".REL")):
-            preprocessed_items = inline_for_REL(container)
-        else:
-            lines_as_list = inline(container)
-            for line in lines_as_list:
-                if line.startswith(".I"):
-                    index_string_to_integer = int(line.split()[1])
-                    preprocessed_items.append({"I": index_string_to_integer})
-                elif line.startswith(".W") or line.startswith(".T"):
-                    reduced_line = line[3:]
-                    processing_item = chain_tasks_by_taskstring(taskstring, reduced_line)
-                    item_index = "W" if line.startswith(".W") else "T"
-                    preprocessed_items[-1][item_index] = processing_item
-                elif line.startswith(".A") or line.startswith(".X"):
-                    line_without_prefix = line[3:].strip()
-                    item_index = "A" if line.startswith(".A") else "X"
-                    preprocessed_items[-1][item_index] = line_without_prefix
-                else:
-                    continue
-
-
-
-        database.save_object(preprocessed_items, taskstring + "_pp_" + filename)
+taskstring_dict = {"t": text_processing.tokenize,
+                   "n": text_processing.normalize,
+                   "w": text_processing.remove_stop_words,
+                   "p": lambda s: text_processing.stemming(s, "porter"),
+                   "l": lambda s: text_processing.stemming(s, "lancaster"),
+                   "v": lambda s: text_processing.stemming(s, "sulyvahn")}
 
 
 def download_NLTK_packages(packages: list) -> None:
@@ -83,10 +23,75 @@ def download_NLTK_packages(packages: list) -> None:
         time.sleep(1)
 
 
-def chain_tasks_by_taskstring(taskstring: str, processing_item) -> list:
+def apply_tasks_by_taskstring(taskstring: str, processing_item: str) -> list:
     result = [processing_item]
     for letter in taskstring:
         result = taskstring_dict[letter](result)
     return result
 
 
+def is_taskstring_valid(taskstring) -> bool:
+    if taskstring == "":
+        return True
+    else:
+        return type(taskstring) == str \
+               and taskstring[0] in taskstring_dict \
+               and is_taskstring_valid(taskstring[1:])
+
+
+def __transform_rel_to_query_to_expected_documents_dictionary(filename):
+    with open(pp.dir_archive + filename, 'r') as container:
+        result = {}
+        for line in container:
+            split = line.split()
+            if len(split > 1):
+                result[split[0]] = result[split[1]] \
+                    if split[0] not in result else result[split[0]].append(split[1])
+        return result
+
+
+def __transform_indexed_file_to_dictionary_of_indexed_parts(taskstring, filename):
+    index_letters, content = [], []
+    with open(pp.dir_archive + filename, 'r') as container:
+
+        for line in container:
+            match_on_index_letter = re.fullmatch('(?s:(\\.)([A-Z])( |\n)(.*))', line)
+
+            if match_on_index_letter:
+                index_letter = match_on_index_letter.__getitem__(2)
+                rest = match_on_index_letter.__getitem__(4)
+
+                if index_letter.strip() == 'I':
+                    rest = int(rest) - 1
+                    index_letters.append([index_letter])
+                    content.append([rest])
+                else:
+                    rest = apply_tasks_by_taskstring(taskstring, rest)
+                    index_letters[-1].append(index_letter)
+                    content[-1].append(rest)
+
+            elif content:
+                content[-1][-1] += line
+
+    # map indeces to content and turn into dictionary for every pair of indeces, content lists
+    # returns a list of dictionary
+    return [dict(zip(i, r)) for i, r in zip(index_letters, content)]
+
+
+def __simple_preprocess(taskstring, filename):
+    with open(pp.dir_archive + filename, 'r') as container:
+        big_string = " ".join(container.readlines())
+    return apply_tasks_by_taskstring(taskstring, big_string)
+
+
+def pre_processor(taskstring: str, filename: str) -> None:
+    preprocessed_items = []
+    # for relation files
+    if filename.endswith(".REL"):
+        preprocessed_items = __transform_rel_to_query_to_expected_documents_dictionary(filename)
+    # for indexed files
+    elif filename.endswith(".ALL") or filename.endswith(".QRY"):
+        preprocessed_items = __transform_indexed_file_to_dictionary_of_indexed_parts(taskstring, filename)
+    else:
+        __simple_preprocess(taskstring, filename)
+    database.save_object(preprocessed_items, taskstring + "_pp_" + filename)
