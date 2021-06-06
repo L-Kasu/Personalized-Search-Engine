b49553e48ffef81e0b893c5dbfa7722cdd5e17de
musicalwillow <83435286+musicalpillow@users.noreply.github.com>
2021-06-06 03:17:11 +0200

    changing from pack() to grid() and enhancing design

--- ui_builder3.py
+++ ui_builder3.py
@@ -36,74 +36,113 @@ def user_entry():
     return search_entry.get()
 
 
+# clears the entry box
+def delete():
+    search_entry.delete(0, END)
+
+
 # instantiating a Window:
 window = Tk()
-window.geometry("800x400")
+window.geometry("810x400")
 window.title("Search Engine")
 
 # Allows the user to select the directory they want to search in
 select_directory = Button(window,
                           text="Select Directory",
                           command=select_dir)
-select_directory.pack(side=TOP, anchor=NW)
+select_directory.grid(row=0, column=0)
+
+# empty space
+empty_1 = Label(window, text="     ")
+empty_1.grid(row=0, column=1)
 
 # Used many frames here to organize the different widgets better
 # a settings frame including: options for selecting file size and type
-settings_frame = Frame(window)
-settings_frame.pack(side=TOP)
+settings_frame = Frame(window, relief=RAISED, bd=5)
+settings_frame.grid(row=0, column=2)
 
 settings_label = Label(settings_frame,
                        text="Settings",
-                       font=("Arial", 20, "bold"))
-settings_label.pack()
+                       font=("Arial", 15, "bold"))
+settings_label.grid(row=0, column=1)
 
+# File size
 file_size_frame = Frame(settings_frame)
-file_size_frame.pack()
+file_size_frame.grid(row=1, column=1)
 
 file_size_label = Label(file_size_frame,
-                        text="Pick file size (in MB)",
+                        text="Select file size (in MB)",
                         font=("Arial", 10, "bold"))
-file_size_label.pack()
+file_size_label.grid(row=0, column=0)
 
 file_size_scale = Scale(settings_frame,
                         from_=0,
                         to=200,
                         length=150,
                         orient=HORIZONTAL)
-file_size_scale.pack()
+file_size_scale.grid(row=2, column=1)
 
+# File type
 file_type_frame = Frame(settings_frame)
-file_type_frame.pack()
+file_type_frame.grid(row=3, column=1)
 
 file_type_label = Label(file_type_frame,
                         text="Select file type:",
                         font=("Arial", 10, "bold"))
-file_type_label.pack()
+file_type_label.grid(row=4, column=1)
 
 pdf = IntVar()
 file_type_pdf = Checkbutton(file_type_frame, text="PDF", variable=pdf)
-file_type_pdf.pack(side=LEFT)
+file_type_pdf.grid(row=5, column=0)
 
 txt = IntVar()
 file_type_txt = Checkbutton(file_type_frame, text="TXT", variable=txt)
-file_type_txt.pack(side=LEFT)
+file_type_txt.grid(row=5, column=1)
 
 docx = IntVar()
 file_type_docx = Checkbutton(file_type_frame, text="DOCX", variable=docx)
-file_type_docx.pack(side=LEFT)
+file_type_docx.grid(row=5, column=2)
 
 preprocess_button = Button(settings_frame, text="preprocess", command=preprocess)
-preprocess_button.pack(side=BOTTOM)
+preprocess_button.grid(row=6, column=1)
+
+# empty space
+empty_2 = Label(window, text="     ")
+empty_2.grid(row=0, column=3)
 
 # Entry frame
 entry_frame = Frame(window)
-entry_frame.pack()
+entry_frame.grid(row=0, column=4)
 
 search_entry = Entry(entry_frame)
-search_entry.pack(side=TOP)
+search_entry.grid(row=0, column=4, ipadx=80, ipady=10)
 
-search_button = Button(entry_frame, text="Search")
-search_button.pack(side=BOTTOM)
+# Buttons frame
+buttons_frame = Frame(entry_frame)
+buttons_frame.grid(row=1, column=4)
 
+search_button = Button(buttons_frame, text="Search")
+search_button.grid(row=1, column=4)
+delete_button = Button(buttons_frame, text="Clear", command=delete)
+delete_button.grid(row=1, column=5)
 
+# empty space
+empty_3 = Label(window, text=" ")
+empty_3.grid(row=2, column=0)
+
+# Path frame
+path_frame = Frame(window)
+path_frame.grid(row=3, column=0)
+path_label = Label(path_frame,
+                   text="The path to the resulted document(s):",
+                   font=("Arial", 10, "bold"))
+path_label.grid(row=4, column=0)
+
+# Result frame
+result_frame = Frame(window)
+result_frame.grid(row=3, column=2)
+result_label = Label(result_frame,
+                     text="Search result:",
+                     font=("Arial", 10, "bold"))
+result_label.grid(row=4, column=1)
 window.mainloop()
