7c42881d80e3dff542dd0317d12f08a5b47cc9b7
JuliBD <83228693+JuliBD@users.noreply.github.com>
2021-06-15 14:50:32 +0200

    Create spanish.py

---
+++ ui_languagepacks/spanish.py
@@ -0,0 +1,12 @@
+# spanish
+
+txt_mastertitle = "Search Engine"
+txt_selectdir = "Seleccionar Directorio"
+txt_settingsheader = "Ajustes"
+txt_settingsfilesize = "Seleccionar el tamaño del archivo (en MB)"
+txt_settingsfiletype = "Seleccionar tipo de archivo"
+txt_preprocess = "preprocesar"
+txt_entrysearch = "Buscar"
+txt_entryclear = "Borrar"
+txt_resultpath = "Ruta de archivo del resultado"
+txt_resultitems = "Buscar en resultados"
