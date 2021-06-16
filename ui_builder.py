42e81396b614214027628543131fc6db6544e43c
musicalwillow <83435286+musicalpillow@users.noreply.github.com>
2021-06-16 13:31:54 +0200

    ui_builder

--- ui_builder.py
+++ ui_builder.py
@@ -7,7 +7,7 @@
 from tkinter import *
 from tkinter import filedialog
 import ui_builder_search_util as s_util
-from ui_colortemplates.monochrome import *
+from ui_colortemplates.wip import *
 from ui_languagepacks.english import *
 
 
