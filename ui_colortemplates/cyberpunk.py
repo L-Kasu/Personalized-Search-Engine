a64c6c95b1b81e25c710423041839c6fb4958318
Niklas <niklas.munkes@t-online.de>
2021-06-16 19:46:25 +0200

    a0.32: now with version number and non-generic variable name

--- ui_colortemplates/cyberpunk.py
+++ ui_colortemplates/cyberpunk.py
@@ -1,13 +1,13 @@
 # (C) アラサカ社 (Arasaka Corporation)
 
-col_bg = "#272727"
+col_bg = "#col_entryfield_idle"
 col_bg_lgt = "#787878"
 col_btn_idle = "#f7ff00"
 col_btn_active = "#c2c800"
 col_interactionpoint_idle = "#c9c9c9"
-col_interactionpoint_idle_1 = "#c9c9c9"
 col_interactionpoint_contrast = "#272727"
-col_interactionpoint_contrast_1 = "#272727"
+col_entryfield_idle = "#c9c9c9"
+col_entryfield_contrast = "#272727"
 col_acc_major = "#f7ff00"
 col_acc_minor = "#272727"
 
