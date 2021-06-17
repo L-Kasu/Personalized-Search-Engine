ff84a413d72f2f991451e92c92996c3a0d2ce498
L-Kasu <lars@kasueschke.de>
2021-06-17 19:07:16 +0200

    can now read in pdfs and search in them

--- ui_builder_search_util.py
+++ ui_builder_search_util.py
@@ -9,7 +9,40 @@ from tkinter import filedialog
 from preprocessing import pp_main as pm
 from data import database
 import tf
+import io
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                  password=password,
+                                  caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+
+
+    fp.close()
+    device.close()
+    text = retstr.getvalue()
+    retstr.close()
+    return text
 
 
 # initiates the searching algorithm
@@ -26,6 +59,11 @@ def any_file_to_str(path):
     if path.endswith("txt"):
         with open(path, "r") as container:
             text = container.read()
+    elif path.endswith("pdf"):
+        text = convert_pdf_to_txt(path)
+    else:
+        text = ""
+        print("unsupported file format at:" + path)
     return text
 
 
