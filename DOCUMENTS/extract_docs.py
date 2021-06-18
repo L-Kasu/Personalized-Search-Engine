295e8f1f4018f2d99ed17261428fb206913a494e
Niklas <niklas.munkes@t-online.de>
2021-06-18 08:51:59 +0200

    a0.44: added a small txt folder for testing

---
+++ DOCUMENTS/extract_docs.py
@@ -0,0 +1,11 @@
+with open("./CISI_ALL.txt", "r") as container:
+    title = "default"
+    for line in container.readlines():
+        if line.startswith(".T"):
+            title = line[3:].rstrip("\n")
+        elif line.startswith(".W"):
+            paragraph = line[3:]
+            document = open("./doc_folder/" + title + ".txt", 'w')
+            document.write(paragraph)
+            document.close()
+container.close()
