187218807ca64d29a4164d0f02adb19df1dee1d3
musicalwillow <83435286+musicalpillow@users.noreply.github.com>
2021-06-29 15:48:24 +0200

    Update popup_window.py

--- gui/builder_toolbox/tkinter_objects/popup_window.py
+++ gui/builder_toolbox/tkinter_objects/popup_window.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 
 def popup_window(self):
-    self.win = Toplevel(self)
+    self.win = Toplevel()
     self.win.wm_title("Preview")
 
     self.prev_label = Label(self.win, text="here will the preview appear")
