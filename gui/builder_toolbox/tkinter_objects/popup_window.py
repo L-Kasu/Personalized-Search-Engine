32e7695bb48dde8f14def5e8192c89fce3747a4a
musicalwillow <83435286+musicalpillow@users.noreply.github.com>
2021-06-29 15:46:56 +0200

    Update popup_window.py

--- gui/builder_toolbox/tkinter_objects/popup_window.py
+++ gui/builder_toolbox/tkinter_objects/popup_window.py
@@ -5,8 +5,8 @@ def popup_window(self):
     self.win = Toplevel(self)
     self.win.wm_title("Preview")
 
-    self.label_prev = Label(self.win, text="here will the preview appear")
-    self.label_prev.pack(TOP)
+    self.prev_label = Label(self.win, text="here will the preview appear")
+    self.prev_label.pack(TOP)
 
     self.exit_btn = Button(self.win, text="Okay", command=self.win.destroy())
     self.exit_btn.pack(BOTTOM)
