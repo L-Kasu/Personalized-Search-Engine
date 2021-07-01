d409790883a1257be53bf580ad89f747c6ec4ae1
L-Kasu <lars@kasueschke.de>
2021-07-01 17:11:10 +0200

    distance measure update 1

--- clustering.py
+++ clustering.py
@@ -3,19 +3,22 @@ from kneed import KneeLocator
 from data import database
 import pandas as pd
 import numpy as np
+from tf import tfidf
 
 # TODO: what should  KMAX be?
-KMAX = 8
+# TODO: what should the sensitivity be
+S = 2.0
+KMAX = 50
 
 
-class Clustering():
-    def __init__(self, tf_obj):
-        self.KMAX = 8
-        self.tf_obj = tf_obj
+class Clustering(tfidf):
+    def __init__(self, corpus: list, titles: list):
+        super().__init__(corpus, titles)
+        self.KMAX = KMAX
         self.clustering = self.__kmeans(self.__find_optimal_k(self.KMAX))
 
     def __find_optimal_k(self, kmax):
-        points = self.tf_obj.tfidf_mat
+        points = self.tfidf_mat
         sse = []
         k_list = list(range(1, kmax + 1))
         for k in k_list:
@@ -27,18 +30,20 @@ class Clustering():
             # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
             for i in range(points.shape[0]):
                 curr_center = centroids[pred_clusters[i]]
-                curr_sse += (points[i, 0] - curr_center[0]) ** 2 + (points[i, 1] - curr_center[1]) ** 2
+                vec = points[i,:]
+                curr_sse += np.linalg.norm(vec - curr_center)**2
 
             sse.append(curr_sse)
 
-        elbow_graph = KneeLocator(k_list, sse, S=1.0, curve="convex", direction="decreasing")
+        elbow_graph = KneeLocator(k_list, sse, S=S, curve="convex", direction="decreasing")
         optimal_k = elbow_graph.elbow
+        if not optimal_k:
+            optimal_k = 1
         # TODO: make plotting work
-        elbow_graph.plot_knee()
         return optimal_k
 
     def __kmeans(self, k):
-        clustering = KMeans(n_clusters=k).fit(self.tf_obj.tfidf_mat)
+        clustering = KMeans(n_clusters=k).fit(self.tfidf_mat)
         return clustering
 
     def get_cluster_of_vector(self, vec):
@@ -47,11 +52,11 @@ class Clustering():
     def get_cluster_of_index(self, k):
         titles = []
         vecs = []
-        for i in range(0, self.tf_obj.tfidf_mat.shape[0]):
-            vec = self.tf_obj.tfidf_mat[i]
+        for i in range(0, self.tfidf_mat.shape[0]):
+            vec = self.tfidf_mat[i]
             if self.get_cluster_of_vector(vec) == k:
-                titles.append(self.tf_obj.titles[i])
-                vecs.append(self.tf_obj.tfidf_mat[i])
+                titles.append(self.titles[i])
+                vecs.append(self.tfidf_mat[i])
         vecs = np.array(vecs)
         return (titles, vecs)
 
@@ -61,7 +66,9 @@ class Clustering():
 
 def process_example_clustering():
     tf_obj = database.load_object("doc_folder")
-    clustering = Clustering(tf_obj)
+    titles = tf_obj.titles
+    corpus = tf_obj.corpus
+    clustering = Clustering(corpus, titles)
     clustering_at_two = clustering.get_cluster_of_index(0)
     print("end")
 
