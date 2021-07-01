73adff8071865003addeb5d9fbbd019ad2d97b46
KarylReyne <niklas.munkes@t-online.de>
2021-07-01 22:54:56 +0200

    code location adjustments

--- gui/builder_toolbox/tkinter_objects/buttons.py
+++ gui/builder_toolbox/tkinter_objects/buttons.py
@@ -1,14 +1,9 @@
-import os
 from tkinter import filedialog
-import tf
-from data import database
 from gui.builder_toolbox.search_util import *
-from gui.builder_toolbox.tkinter_objects.labels import dir_label
+from gui.builder_toolbox.tkinter_objects.labels import *
 from gui.builder_toolbox.tkinter_objects.textboxes import *
 from gui.languagepacks.English import *
 from gui.colortemplates.wip import *
-from gui.builder_toolbox import search_util
-import clustering
 
 
 preview_size = 300
@@ -101,36 +96,3 @@ def preview_function(self, n):
 
     self.preview_window_label = None
     preview_window_text(self, self.preview_window, text)
-
-
-def search(self, query):
-    __preprocess(self)
-    self.result_text.delete(0, self.result_text.size())
-    tf_obj = self.tf_object
-    query_vec = tf_obj.tfidfVectorizer.transform([query])
-    cluster_index = tf_obj.get_cluster_of_vector(query_vec)
-    corpus, titles, vecs = tf_obj.get_cluster_of_index(cluster_index)
-    tf_copy = clustering.Clustering(corpus, titles)
-    return_docs_num = 10
-
-    if tf_copy:
-        result = tf_copy.query_k_titles(query, return_docs_num)
-        for x in range(0, len(result)):
-            self.result_text.insert(x, result[x])
-
-
-def __preprocess(self):
-    corpus_list = []
-    titles = []
-    for _, _, filenames in os.walk(self.dir_selected):
-        titles = filenames
-        dir = os.path.basename(self.dir_selected)
-        for filename in filenames:
-            path = self.dir_selected + "/" + filename
-            text = search_util.any_file_to_str(path)
-            corpus_list.append(text)
-        # TODO: implement saving to databases
-
-        if titles and corpus_list:
-            self.tf_object = clustering.Clustering(corpus_list, titles)
-        break
