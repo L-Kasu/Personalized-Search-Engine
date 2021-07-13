138406f5eca2af7b41596fe1a897ac1d2e6d5f6e
KarylReyne <niklas.munkes@t-online.de>
2021-07-13 20:59:26 +0200

    ported ALL color and language templates to the new settings system

--- executable.py
+++ executable.py
@@ -4,6 +4,8 @@ from gui.ui_builder import Application
 
 if __name__ == "__main__":
     init_config()
+    # set_language("german")
+    # set_colors("sharky")
     root = Tk()
     app = Application(master=root)
     app.mainloop()
