a693c06e15520664f605859d60f475f6b269ad51
KarylReyne <niklas.munkes@t-online.de>
2021-07-23 13:50:49 +0200

    fixed stemmer selection bug

--- search/search_methods.py
+++ search/search_methods.py
@@ -73,14 +73,15 @@ class TfidfMethod(SearchMethod):
     def txt_to_vec(self, txt):
         return self.tfidfVectorizer.transform([txt])
 
-    def get_stems(text,
-                  snowball_language=get_config("snowballstemmer_language"),
-                  configstemmer=get_config("stemmer")):
-        snowball_language = snowball_language
-        stemmerdict = {"porter": PorterStemmer(),
-                       "lancaster": LancasterStemmer(),
-                       "snowball": snowball.SnowballStemmer(snowball_language)}
-        stemmer = stemmerdict[configstemmer]
-        tokens = [word for word in nltk.word_tokenize(text) if len(word) > 1]
-        stems = [stemmer.stem(item) for item in tokens]
-        return stems
+
+def get_stems(text,
+              snowball_language=get_config("snowballstemmer_language"),
+              configstemmer=get_config("stemmer")):
+    snowball_language = snowball_language
+    stemmerdict = {"porter": PorterStemmer(),
+                   "lancaster": LancasterStemmer(),
+                   "snowball": snowball.SnowballStemmer(snowball_language)}
+    stemmer = stemmerdict[configstemmer]
+    tokens = [word for word in nltk.word_tokenize(text) if len(word) > 1]
+    stems = [stemmer.stem(item) for item in tokens]
+    return stems
