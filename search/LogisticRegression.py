59b6d643b3a7608641d8b1dbb5fd0489c7971bf3
L-Kasu <lars@kasueschke.de>
2021-07-30 12:02:18 +0200

    first "working" model

--- search/LogisticRegression.py
+++ search/LogisticRegression.py
@@ -8,11 +8,11 @@ from nltk.tokenize import word_tokenize
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-class Model():
-    def __init__(self, feature_creating_functions):
+class Model:
+    def __init__(self, feature_creating_functions, class_weight="balanced"):
         self.training_data, self.test_data, self.validation_data = ((), (), ())
         self.feature_generator = feature_creating_functions
-        self.model = LogisticRegression()
+        self.model = LogisticRegression(solver='liblinear', random_state=0, class_weight=class_weight)
 
     def intialise_data(self, documents, query_dict, rel_dict):
         self.training_data, self.test_data, self.validation_data = self.create_datasets(documents, query_dict, rel_dict)
@@ -26,20 +26,34 @@ class Model():
         return self.model.score(X, y)
 
     def test(self):
-        pass
+        negative = []
+        positive = []
+        test_data = self.test_data
+        for i in range(len(test_data[0])):
+            item = test_data[0][i]
+            predicted = self.score(np.array(item).reshape(1, -1))
+            actual = test_data[1][i]
+            if actual == 1:
+                positive.append(predicted[1])
+            else:
+                negative.append(predicted[0])
+            print(" query: ", test_data[3][i],
+                  " doc: ", test_data[2][i],
+                  " predicted: ", predicted,
+                  " actual : ", actual)
+        print("negative:", sum(negative)/len(negative), "positive:", sum(positive)/len(positive))
 
-    def score(self, doc1, doc2):
-        doc_features = [func(doc1, doc2) for func in self.feature_generator]
-        return self.model.predict_proba(doc_features)[0][0]
+
+    def score(self, doc_features):
+        return self.model.predict_proba(doc_features)[0]
 
     # return the matrix of features, by applying every function by the list for one feature, and the corresponding labels
     def create_datasets(self, documents, query_dict, rel_dict):
         train = ([], [])
         test = ([], [], [], [])
         validate = ([], [])
-        counter = 1
-        for j in range(0, len(documents)):
-            for i in range(0, len(query_dict)):
+        for i in range(0, len(query_dict)):
+            for j in range(0, len(documents)):
                 if i in rel_dict:
                     label = 1 if j in rel_dict[i] else 0
                 else:
@@ -62,40 +76,12 @@ class Model():
         return train, test, validate
 
 
-def main():
-    Vectorizer = TfidfVectorizer(analyzer='word')
-    embedding = load_embedding()
-    scores = []
-    for i in range(0,5):
-        # create list of features we want to take into consideration
-        embedding_vectors_distance = lambda doc1: lambda doc2: \
-            np.linalg.norm(document_to_embedding_vector(embedding, doc1) - document_to_embedding_vector(embedding, doc2))
-        embedding_vectors_cosine = lambda doc1: lambda doc2: \
-            cosine_similarity(document_to_embedding_vector(embedding, doc1).reshape(1, -1),
-                              document_to_embedding_vector(embedding, doc2).reshape(1, -1))[0][0]
-        rnd = lambda _: lambda _: random.uniform(0, 1)
-        tfidf_cosine = lambda doc1: lambda doc2: tfidf_cos_sim(Vectorizer, doc1, doc2)
-        # making sure the validation runs correctly, by using a random funcition for testing and we should get back .5
-        function_list = [tfidf_cosine, embedding_vectors_distance, embedding_vectors_cosine]
-        documents = file_reader.load_all()[2]
-        query_dict = file_reader.load_qry()
-        rel_dict = {}
-        with open("../data/tn_pp_CISI.REL.pickle", "rb") as f:
-            rel_dict = pickle.load(f)
-        model = Model(function_list)
-        model.intialise_data(documents, query_dict, rel_dict)
-        model.train()
-        score = model.validate()
-        print(score)
-        scores.append(score)
-    print(sum(scores)/len(scores))
-
-
 def document_to_embedding_vector(embedding, txt: str):
     word_embedding = embedding
     split_str = word_tokenize(txt.lower())
     vector = np.mean([word_embedding[word] for word in split_str if word in word_embedding], axis=0)
     return vector
+
 
 def tfidf_cos_sim(Vectorizer, doc1, doc2):
     vectorized = Vectorizer.fit_transform([doc1, doc2])
@@ -111,6 +97,32 @@ def load_embedding():
     word_embedding = pickle.load(open(path, "rb"))
     return word_embedding
 
+def main():
+    Vectorizer = TfidfVectorizer(analyzer='word', stop_words="english")
+    embedding = load_embedding()
+    scores = []
+    # create list of features we want to take into consideration
+    embedding_vectors_distance = lambda doc1: lambda doc2: \
+        1/(np.linalg.norm(document_to_embedding_vector(embedding, doc1) - document_to_embedding_vector(embedding, doc2)))
+    embedding_vectors_cosine = lambda doc1: lambda doc2: \
+        cosine_similarity(document_to_embedding_vector(embedding, doc1).reshape(1, -1),
+                          document_to_embedding_vector(embedding, doc2).reshape(1, -1))[0][0]
+    rnd = lambda _: lambda _: random.uniform(0, 1)
+    tfidf_cosine = lambda doc1: lambda doc2: tfidf_cos_sim(Vectorizer, doc1, doc2)
+    # making sure the validation runs correctly, by using a random funcition for testing and we should get back .5
+    function_list = [tfidf_cosine, embedding_vectors_cosine]
+    documents = file_reader.load_all()[2][:100]
+    query_dict = file_reader.load_qry()
+    rel_dict = {}
+    with open("../data/tn_pp_CISI.REL.pickle", "rb") as f:
+        rel_dict = pickle.load(f)
+    my_model = Model(function_list)
+    my_model.intialise_data(documents, query_dict, rel_dict)
+    my_model.train()
+    score = my_model.validate()
+    my_model.test()
+    print(score)
+    scores.append(score)
 
 if __name__ == "__main__":
     main()
