6b2f29a3a73cf0d690d028f8976ea59076b71f0d
L-Kasu <lars@kasueschke.de>
2021-08-02 13:21:11 +0200

    trains now on doc - doc relation instead of doc - query

--- search/LogisticRegression.py
+++ search/LogisticRegression.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import random, pickle, os
+import sys
 import numpy as np
 from evaluation import file_reader
 from nltk.tokenize import word_tokenize
@@ -14,10 +15,14 @@ class Model:
     def __init__(self, feature_creating_functions, class_weight="balanced"):
         self.training_data, self.test_data, self.validation_data = ((), (), ())
         self.feature_generator = feature_creating_functions
-        self.model = LogisticRegression(solver='saga', random_state=0, class_weight=class_weight)
+        self.model = LogisticRegression( random_state=0, class_weight=class_weight)
 
-    def intialise_data(self, documents, query_dict, rel_dict):
-        self.training_data, self.test_data, self.validation_data = self.create_datasets(documents, query_dict, rel_dict)
+    # prepares datasets to train the model:
+    # - docs_1: first list of documents
+    # - docs_2: second list of documets
+    # - rel_dicts: mapping from indeces of docs_1 to docs_2 if they are related
+    def intialise_data(self, docs_1, docs_2, rel_dict):
+        self.training_data, self.test_data, self.validation_data = self.create_datasets(docs_1, docs_2, rel_dict)
 
     def train(self):
         X, y = self.training_data
@@ -28,49 +33,28 @@ class Model:
         return self.model.score(X, y)
 
     def test(self):
-        wrong_unrelated = []
-        right_unrelated = []
-        wrong_related = []
-        right_related = []
-
-        test_data = self.test_data
-        for i in range(len(test_data[0])):
-            item = test_data[0][i]
-            predicted = self.score(np.array(item).reshape(1, -1))
-            actual = test_data[1][i]
-            if actual == 1:
-                if predicted[1] > .5:
-                    right_related.append(predicted[1])
-                else:
-                    wrong_unrelated.append(predicted[1])
-            else:
-                if predicted[1] > .5:
-                    wrong_related.append(predicted[1])
-                else:
-                    right_unrelated.append(predicted[1])
-
-        print("right related: ", len(right_related),
-              "wrong related: ", len(wrong_related),
-              "right unrelated: ", len(right_unrelated),
-              "wrong unrelated: ", len(wrong_unrelated),)
+        pass
 
 
 
-    def score(self, doc_features):
-        return self.model.predict_proba(doc_features)[0]
+
+
+    def score(self, query, doc):
+        feature = [func(query)(doc) for func in self.feature_generator]
+        return self.model.predict_proba(feature)[0]
 
     # return the matrix of features, by applying every function by the list for one feature, and the corresponding labels
-    def create_datasets(self, documents, query_dict, rel_dict):
+    def create_datasets(self, docs_1, docs_2, rel_dict):
         train = ([], [])
         test = ([], [], [], [])
         validate = ([], [])
-        for i in range(0, len(query_dict)):
-            for j in range(0, len(documents)):
+        for i in range(0, len(docs_2)):
+            for j in range(0, len(docs_1)):
                 if i in rel_dict:
                     label = 1 if j in rel_dict[i] else 0
                 else:
                     label = 0
-                sample = [func(query_dict[i])(documents[j]) for func in self.feature_generator]
+                sample = [func(docs_2[i])(docs_1[j]) for func in self.feature_generator]
                 query_number, doc_number = (str(i + 1), str(j + 1))
                 rnd = random.uniform(0, 1)
                 if rnd <= .6:
@@ -84,7 +68,7 @@ class Model:
                 else:
                     validate[0].append(sample)
                     validate[1].append(label)
-        print("the share of related documents: sum(train[1])/len(train[1])")
+        print("the share of related documents:", sum(train[1])/len(train[1]))
         return train, test, validate
 
 
@@ -124,32 +108,31 @@ def main():
     scores = []
     # create list of features we want to take into consideration
     embedding_vectors_distance = lambda doc1: lambda doc2: \
-        1/(np.linalg.norm(document_to_embedding_vector(embedding, doc1) - document_to_embedding_vector(embedding, doc2)))
+        1/((np.linalg.norm(document_to_embedding_vector(embedding, doc1) - document_to_embedding_vector(embedding, doc2))) + sys.float_info.epsilon)
     embedding_vectors_cosine = lambda doc1: lambda doc2: \
         cosine_similarity(document_to_embedding_vector(embedding, doc1).reshape(1, -1),
                           document_to_embedding_vector(embedding, doc2).reshape(1, -1))[0][0]
     rnd = lambda _: lambda _: random.uniform(0, 1)
     tfidf_cosine = lambda doc1: lambda doc2: tfidf_cos_sim(Vectorizer, doc1, doc2)
     # making sure the validation runs correctly, by using a random funcition for testing and we should get back .5
-    function_list = [embedding_vectors_distance, embedding_vectors_cosine, tfidf_cosine]
-    documents = file_reader.load_all()[2][:documents_to_use]
-    query_dict = file_reader.load_qry()
-    rel_dict = {}
+    function_list = [tfidf_cosine, embedding_vectors_distance, embedding_vectors_cosine]
+    library = file_reader.load_all()
+    docs_1 = library[2][:100]
+    docs_2 = docs_1
+    rel_dict = library[3]
     my_model = None
-    with open("../data/tn_pp_CISI.REL.pickle", "rb") as f:
-        rel_dict = pickle.load(f)
     # if load:
     #   with open("./my_model.pickle", "rb") as f:
     #       my_model = pickle.load(f)
     # else:
     #    with open("./my_model.pickle", "wb") as f:
     my_model = Model(function_list)
-    my_model.intialise_data(documents, query_dict, rel_dict)
+    my_model.intialise_data(docs_1, docs_2, rel_dict)
     #        pickle.dump(my_model, f, protocol=pickle.HIGHEST_PROTOCOL)
     my_model.train()
     score = my_model.validate()
-    my_model.test()
     print(score)
+    my_model.test()
     scores.append(score)
 
 if __name__ == "__main__":
