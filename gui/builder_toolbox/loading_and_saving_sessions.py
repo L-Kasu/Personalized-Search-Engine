67fa78614ca83a0d1bf28f423cb6482d887ed369
L-Kasu <lars@kasueschke.de>
2021-08-04 21:13:51 +0200

    basic exception handling and added files getting closed

--- gui/builder_toolbox/loading_and_saving_sessions.py
+++ gui/builder_toolbox/loading_and_saving_sessions.py
@@ -7,11 +7,16 @@ previous_session_path = "config/session/previous_session.pickle"
 def load_session(path=previous_session_path):
     directory_path = ""
     search_class_instance = None
-    if os.path.isfile(path):
-        directory_path, search_class_instance = pickle.load(open(path, "rb"))
+
+    with open(path, "rb") as file:
+        try:
+            directory_path, search_class_instance = pickle.load(file)
+        except EOFError:
+            pass
     return directory_path, search_class_instance
 
 
 def save_session(directory_path, search_class_instance, path=previous_session_path):
-    pickle.dump((directory_path, search_class_instance), open(path, "wb"))
+    with open(path, "wb") as file:
+        pickle.dump((directory_path, search_class_instance), file)
     pass
