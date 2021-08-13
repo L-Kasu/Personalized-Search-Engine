d07b8f6a5c2861fd455b9d7eb67ee7f9492358d1
Niklas <niklas.munkes@t-online.de>
2021-08-13 11:10:36 +0200

    better splash.png

--- createDist.py
+++ createDist.py
@@ -8,5 +8,10 @@ PyInstaller.__main__.run([
     '--workpath=./dist/windows-build',
     '--splash=splash.png',
     '--name=Magni',
-    '--icon=icon.ico'
+    '--icon=icon.ico',
+    # '--add-data=./config;./dist/windows-dist/config',
+    # '--add-data=./icon.ico;./dist/windows-dist/icon.ico',
+    # '--add-data=./splash.png;./dist/windows-dist/splash.png',
+    # '--add-data=./LICENSE.md;./dist/windows-dist/LICENSE.md',
+    # '--add-data=./README.md;./dist/windows-dist/README.md',
 ])
